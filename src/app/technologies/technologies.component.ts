0c2b41c3a3d139a4436236ffccac502f77acab23
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2021-04-11 16:35:28 -0500

    included arrow controls to carousel

--- src/app/technologies/technologies.component.ts
+++ src/app/technologies/technologies.component.ts
@@ -6,8 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./technologies.component.css']
 })
 export class TechnologiesComponent implements OnInit {
+  public imageLinks = [
+    "../../assets/Technologies_logos/java.svg.png",
+    "../../assets/Technologies_logos/js.svg.png"
+  ]
 
+  public currentCarouselIndex = 0; 
   constructor() { }
+
+  nextImage() {
+    if (this.currentCarouselIndex === this.imageLinks.length -1) {
+      this.currentCarouselIndex = 0
+    } else {
+      this.currentCarouselIndex += 1;
+    }
+  }
+
+  previousImage() {
+    if (this.currentCarouselIndex === 0) {
+      this.currentCarouselIndex = this.imageLinks.length -1;
+    } else {
+      this.currentCarouselIndex -= 1;
+    }
+  }
 
   ngOnInit(): void {
   }
