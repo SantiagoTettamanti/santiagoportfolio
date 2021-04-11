be6361971634825498149f328eb1ccb1aa4e5eb2
SantiagoTettamanti <SantiagoTettamanti@users.noreply.github.com>
2021-04-11 21:38:42 +0000

    Apply formatting changes

--- src/app/technologies/technologies.component.ts
+++ src/app/technologies/technologies.component.ts
@@ -7,16 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechnologiesComponent implements OnInit {
   public imageLinks = [
-    "../../assets/Technologies_logos/java.svg.png",
-    "../../assets/Technologies_logos/js.svg.png"
-  ]
+    '../../assets/Technologies_logos/java.svg.png',
+    '../../assets/Technologies_logos/js.svg.png',
+  ];
 
-  public currentCarouselIndex = 0; 
-  constructor() { }
+  public currentCarouselIndex = 0;
+  constructor() {}
 
   nextImage() {
-    if (this.currentCarouselIndex === this.imageLinks.length -1) {
-      this.currentCarouselIndex = 0
+    if (this.currentCarouselIndex === this.imageLinks.length - 1) {
+      this.currentCarouselIndex = 0;
     } else {
       this.currentCarouselIndex += 1;
     }
@@ -24,14 +24,11 @@ export class TechnologiesComponent implements OnInit {
 
   previousImage() {
     if (this.currentCarouselIndex === 0) {
-      this.currentCarouselIndex = this.imageLinks.length -1;
+      this.currentCarouselIndex = this.imageLinks.length - 1;
     } else {
       this.currentCarouselIndex -= 1;
     }
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
-
