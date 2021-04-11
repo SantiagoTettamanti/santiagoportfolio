b712f846c0253e44ca84ec655ef8f12f9935cc9a
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2021-04-11 17:08:39 -0500

    Added automation to carousel Added more techs

--- src/app/technologies/technologies.component.ts
+++ src/app/technologies/technologies.component.ts
@@ -8,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class TechnologiesComponent implements OnInit {
   public imageLinks = [
     '../../assets/Technologies_logos/java.svg.png',
+    '../../assets/Technologies_logos/mongodbround.svg.png',
+    '../../assets/Technologies_logos/nodejs.svg.png',
+    '../../assets/Technologies_logos/express.svg.png',
+    '../../assets/Technologies_logos/ts.svg.png',
+    '../../assets/Technologies_logos/angular.svg.png',
+    '../../assets/Technologies_logos/html.svg.png',
+    '../../assets/Technologies_logos/css.svg.png',
     '../../assets/Technologies_logos/js.svg.png',
+    '../../assets/Technologies_logos/bootstrap.png',
+    '../../assets/Technologies_logos/gitround.svg.png'
   ];
 
   public currentCarouselIndex = 0;
@@ -30,5 +39,9 @@ export class TechnologiesComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    setInterval(() => { 
+      this.nextImage(); 
+    }, 2000);
+  }
 }
